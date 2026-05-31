1d4ad166689b446db1b2238f2ff8202cc07a878a
francogallozo52 <francogallozo52@gmail.com>
2026-05-31 05:16:39 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'finanzas-v5';
+const CACHE = 'finanzas-v6';
 const ASSETS = ['./index.html', './manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
