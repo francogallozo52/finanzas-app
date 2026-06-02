56bfa3cd3ebef7c6533f0f84bb721af55e8f64c2
francogallozo52 <francogallozo52@gmail.com>
2026-06-01 21:40:31 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fico-v4';
+const CACHE = 'fico-v5';
 const ASSETS = ['./index.html', './manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
