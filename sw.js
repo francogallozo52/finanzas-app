2675285a9f813be5da1dce906231573b594d7569
francogallozo52 <francogallozo52@gmail.com>
2026-06-01 21:43:32 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,9 @@
-const CACHE = 'fico-v5';
-const ASSETS = ['./index.html', './manifest.json'];
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
+// Service worker minimo - sin cache, siempre sirve la version mas nueva
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
-  self.clients.claim();
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+    .then(() => self.clients.claim())
+  );
 });
-self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(() => caches.match(e.request))));
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
